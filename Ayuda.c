65ed983a81b5a9f22d91cc495df3865ab495643c
Daniel Boyan R. <daniel.boyan@alu.inspt.utn.edu.ar>
2024-06-05 17:57:07 -0300

    AyudaV1

---
+++ Ayuda.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void calcHsMinSeg(int*,int*,int*);
+
+int main (void){
+    int horas,minutos,segundos,aux;
+    printf("Ingrese segundos: ");
+    scanf("%d",&segundos);
+    aux = segundos;
+    if(segundos>=0){
+        calcHsMinSeg(&horas, &minutos, &segundos);
+        printf("De %d Segundos serian %d Hora(s)  %d Minuto(s) y %d Segundo(s)", aux, horas, minutos, segundos);
+    }else{
+        printf("Valor fuera de rango");
+    }
+}
+
+
+void calcHsMinSeg(int *horas, int *minutos, int *segundos)
+{
+    *horas = *segundos / 3600;
+    *minutos = (*segundos % 3600) / 60;
+    *segundos = *segundos % 60;
+}
